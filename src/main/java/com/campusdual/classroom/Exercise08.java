70944c99b167549cccf9baca9afa5b83c01bc1a6
Pablo <pablobarreiro131@gmail.com>
2025-05-22 11:38:25 +0200

    Ejercicio 8, completado y testeado

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise08 {
 
     public static void main(String[] args) {
-
+        whileLoop(5);
     }
 
     //TODO ↓
@@ -12,6 +12,10 @@ public class Exercise08 {
     // X+1 < Y. El próximo ciclo valdrá: X+2
     // etc.
     public static void whileLoop(int num) {
-
+        int x = 0;
+        while(num>x){
+            System.out.println(x + " < " + num + ". El proximo ciclo valdra: " + (x+1));
+            x++;
+        }
     }
 }
